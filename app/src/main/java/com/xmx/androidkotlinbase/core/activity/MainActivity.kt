f72cc315870dfecdb0996a7d0c310c3edb3249cc
Xu Mengxiang <834489218@qq.com>
2017-05-05 23:48:28 +0800

    Optimize TabLayout

--- app/src/main/java/com/xmx/androidkotlinbase/core/activity/MainActivity.kt
+++ app/src/main/java/com/xmx/androidkotlinbase/core/activity/MainActivity.kt
@@ -79,6 +79,12 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         // 设置标签页底部选项卡
         tabLayout.setupWithViewPager(viewPager)
 
+        tabLayout.getTabAt(0)!!.setIcon(R.mipmap.ic_launcher)
+        tabLayout.getTabAt(1)!!.setIcon(R.mipmap.ic_launcher)
+        tabLayout.getTabAt(2)!!.setIcon(R.mipmap.ic_launcher)
+        tabLayout.getTabAt(3)!!.setIcon(R.mipmap.ic_launcher)
+        tabLayout.getTabAt(4)!!.setIcon(R.mipmap.ic_launcher)
+
         EventBus.getDefault().register(this)
     }
 
